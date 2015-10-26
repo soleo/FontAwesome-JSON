49189637d1485c40532505ec05cb937f4a33eccc
Xinjiang Shao <shaoxinjiang@gmail.com>
2015-10-26 15:17:00 -0500

    add php array for fontawesome, update ReadMe

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # FontAwesome-JSON
 Generate a list of font awesome names as JSON string 
+
+
+## Why I need this?
+
+FontAwesome is great, but it only provide yml(https://github.com/FortAwesome/Font-Awesome/blob/master/src/icons.yml) format for icon list. For server side template, we need something more convienient. 
+
+I used a bash script to always get the latest verison of icon name list and convert it to be JSON or php array for me to use in my current projects. The repo with sync with FontAwesome to always get a fresh and complete list of icons.
+
+## Current FontAwesome Verison
+
+4.4.0 
+
--- generator.sh
+++ generator.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-RET=`curl  https://raw.githubusercontent.com/FortAwesome/Font-Awesome/master/less/icons.less 2>/dev/null | grep "fa" | awk -F":before" '{print $1}' | awk '{sub(/^\.@{fa-css-prefix}-*/,""); print "\"" tolower($1) "\","}'`
+RET=`curl  https://raw.githubusercontent.com/FortAwesome/Font-Awesome/4.4.0-wip/less/icons.less 2>/dev/null | grep "fa" | awk -F":before" '{print $1}' | awk '{sub(/^\.@{fa-css-prefix}-*/,""); print "\"" tolower($1) "\","}'`
 
 FIRSTLINE="{"
 LASTLINE="}"
 
 echo $FIRSTLINE$RET$LASTLINE > FontAwesome.json
+
+PHPFIRSTLINE="\$fontawesome = array("
+PHPLASTLINE=");"
+
+echo $PHPFIRSTLINE$RET$PHPLASTLINE > FontAwesome.php
